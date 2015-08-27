622d6a0b896e85938d3a6d6cb1ec13076d39519b
alice8347 <alice8347@hotmail.com>
2015-08-27 09:23:33 -0400

    First Commit

---
+++ MathTwoAssi/src/MathTwo.java
@@ -0,0 +1,12 @@
+
+public class MathTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int mynumber = 2;
+		double myothernumber = 1.7938;
+		System.out.println("mynumber is " + mynumber);
+		System.out.println("myothernumber is " + myothernumber);
+	}
+
+}
